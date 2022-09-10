94cbf5e87d695feb306a13944db59b255ee458aa
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-10 16:35:15 +0530

    Added: Swap Two numbers

--- src/com/blz/Swap2Numbers/Swap2Numbers.java
+++ src/com/blz/Swap2Numbers/Swap2Numbers.java
@@ -1,9 +1,19 @@
 package com.blz.Swap2Numbers;
-
+import java.util.Scanner;
 public class Swap2Numbers {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to SwapNumbers Program!!");
+		int a,b;
+		System.out.println("enter the Two Number:");
+		Scanner scan = new Scanner(System.in);
+		a = scan.nextInt();
+		b = scan.nextInt();
+		System.out.println("Before Swapping"+a+" "+b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("After Swapping"+a+" "+b);
 
 	}
 
